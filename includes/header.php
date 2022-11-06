b5b83c8b7f2e9716fc1ccb20452d966cf6e0b7c5
zeerafle <vsefareez@gmail.com>
2022-11-06 22:26:47 +0800

    session login atau tidak pada header

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,19 @@
     <div class="header-logo">Journable</div>
     <div class="header-list">
         <ul>
-            <li><a href="login.php">Masuk</a></li>
+            <?php
+            // jika session belum di start
+            if (!isset($_SESSION)) {
+                session_start();
+            }
+
+            // jika sudah login tampilin link keluar begitu juga sebaliknya
+            if (isset($_SESSION['login'])) {
+                echo "<li><a href='includes/logout.php'>Keluar</a></li>";
+            } else {
+                echo "<li><a href='login.php'>Masuk</a></li>";
+            }
+            ?>
             <li>Dark mode: <span class="change">OFF</span></li>
             <li><a href="journalExplorer.php">Pencarian</a></li>
             <li>Kategori</li>
