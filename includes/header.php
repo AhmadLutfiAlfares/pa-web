af4b31cd4eb4923f44cc8be0f8fd292e7757b155
AhmadLutfiAlfares <ahmadlutfialfares7@gmail.com>
2022-11-07 23:44:11 +0800

    pilih login

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
             if (isset($_SESSION['login'])) {
                 echo "<li><a href='includes/logout.php'>Keluar</a></li>";
             } else {
-                echo "<li><a href='login.php'>Masuk</a></li>";
+                echo "<li><a href='pilihLogin.php'>Masuk</a></li>";
             }
             ?>
             <li>Dark mode: <span class="change">OFF</span></li>
