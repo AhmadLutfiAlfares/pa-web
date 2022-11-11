8b0c967e3639f35251a1fc79561b9e412451c6db
Bennyhp <bennyhp0805@gmail.com>
2022-11-11 22:12:56 +0800

    Fix Header (All)

--- includes/header.php
+++ includes/header.php
@@ -2,10 +2,7 @@
     <div class="header-logo"><a href="index.php">Journable</a></div>
     <div class="header-list" id="headerId">
         <ul>
-            <li><a href="browse.php">Jelajahi</a></li>
-            <li>Kategori</li>
-            <li><a href="journalExplorer.php">Pencarian</a></li>
-            <li>Dark mode: <span class="change">OFF</span></li>
+            
             <?php
             // jika session belum di start
             if (!isset($_SESSION)) {
@@ -14,12 +11,15 @@
 
             // jika sudah login tampilin link keluar begitu juga sebaliknya
             if (isset($_SESSION['login'])) {
+                echo "<li><a href='browse.php'>Jelajahi</a></li>";
+                echo "<li>Dark mode: <span class='change'>OFF</span></li>";
                 echo "<li><a href='includes/logout.php'>Keluar</a></li>";
             } else {
+                echo "<li><a href='pilihLogin.php'>Jelajahi</a></li>";
+                echo "<li>Dark mode: <span class='change'>OFF</span></li>";
                 echo "<li><a href='pilihLogin.php'>Masuk</a></li>";
             }
             ?>
-            <!-- <li class="ham-btn"><a href=""><i class="fa fa-bars"></i></a></li> -->
         </ul>
     </div>
     <img class="ham-menu" src="images/icons/hamburger-menu.png" onclick="toggleMenu()">
