fe481177dfe37fdcf97989a4cbc7c7f98efc8373
Bennyhp <bennyhp0805@gmail.com>
2022-11-11 21:26:47 +0800

    fix regis (Penerbit)

--- publisher/register.php
+++ publisher/register.php
@@ -61,42 +61,36 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../stylesheet/style.css"/>
-    <link rel="stylesheet" href="stylesheet/register.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-          integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus&family=Work+Sans&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="../stylesheet/style-header-footer.css" />
+    <link rel="stylesheet" href="stylesheet/register.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus&family=Work+Sans&display=swap" rel="stylesheet" />
     <title>Journable | Jelajahi</title>
 </head>
 
 <body>
-<?php
-include('includes/header.php');
-?>
+    <div class="container-form">
+        <form action="register.php" method="post">
+            <label for="name">Nama Institusi</label>
+            <input type="text" name="name">
+            <label for="website">Situs Web</label>
+            <input type="text" name="website">
+            <label for="email">Email</label>
+            <input type="email" name="email" placeholder="example@domain.org">
+            <label for="password">Password</label>
+            <input type="password" name="password" placeholder="Password">
+            <label for="konfir-password">Konfirmasi Password</label>
+            <input type="password" name="konfir-password">
+            <input type="submit" name="submit" value="submit">
+        </form>
 
-<div class="container-form">
-    <form action="register.php" method="post">
-        <label for="name">Nama Institusi</label>
-        <input type="text" name="name">
-        <label for="website">Situs Web</label>
-        <input type="text" name="website">
-        <label for="email">Email</label>
-        <input type="email" name="email" placeholder="example@domain.org">
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="Password">
-        <label for="konfir-password">Konfirmasi Password</label>
-        <input type="password" name="konfir-password">
-        <input type="submit" name="submit" value="submit">
-    </form>
+        <p>Sudah punya akun? <a href="../loginPenerbit.php">Login Sekarang</a></p>
+    </div>
 
-    <p>Sudah punya akun? <a href="../loginPenerbit.php">Login Sekarang</a></p>
-</div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-<script src="../javascript/darkMode.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="../javascript/darkMode.js"></script>
 </body>
 
 </html>
