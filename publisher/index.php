5d66bf1b0a0ee5c6bb6699bb260505f36cc797bf
Bennyhp <bennyhp0805@gmail.com>
2022-11-12 13:32:32 +0800

    Fix Index.php (Publisher) nambah css index.css di stylesheet Publisher

--- publisher/index.php
+++ publisher/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var mysqli $db
  */
@@ -24,31 +25,30 @@ $numOfJournal = mysqli_num_rows($query);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../stylesheet/style-header-footer.css"/>
-    <!--    <link rel="stylesheet" href="stylesheet/index.css"/>-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-          integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus&family=Work+Sans&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="../stylesheet/style-header-footer.css" />
+    <link rel="stylesheet" href="stylesheet/index.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus&family=Work+Sans&display=swap" rel="stylesheet" />
     <title>Journable | Jelajahi</title>
 </head>
 
 <body>
-<?php
-include('includes/header.php');
+    <?php
+    include('includes/header.php');
 
-date_default_timezone_set("Asia/Makassar");
-$timeOfDay = date('a');
-if ($timeOfDay == 'am') {
-    echo 'Good morning, welcome to our site';
-} else {
-    echo 'Good afternoon, welcome to our site';
-}
+    date_default_timezone_set("Asia/Makassar");
+    $timeOfDay = date('a');
+    if ($timeOfDay == 'am') {
+        echo '<p class="welcome">Good morning, welcome to our site</p>';
+    } else {
+        echo '<p class="welcome">Good afternoon, welcome to our site</p>';
+        echo "<p class='pIndex'>Jurnal anda yang sudah di publish: $numOfJournal</p>";
+    }
 
-echo "Jurnal anda yang sudah di publish: $numOfJournal"
-?>
+    ?>
 
 </body>
+
 </html>
