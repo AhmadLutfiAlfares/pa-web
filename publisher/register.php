5c9a95ad9e969bc427e82958f7f0d588f34aa961
zeerafle <vsefareez@gmail.com>
2022-11-12 18:15:00 +0800

    password validation di register penerbit

--- publisher/register.php
+++ publisher/register.php
@@ -61,39 +61,58 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../stylesheet/style-header-footer.css" />
-    <link rel="stylesheet" href="stylesheet/register.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus&family=Work+Sans&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="../stylesheet/style-header-footer.css"/>
+    <link rel="stylesheet" href="stylesheet/register.css"/>
+    <link rel="stylesheet" href="../stylesheet/password-validation.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+          integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus&family=Work+Sans&display=swap" rel="stylesheet"/>
     <title>Journable | Jelajahi</title>
 </head>
 
 <body>
-    <?php
-    include('includes/header.php');
-    ?>
-    <div class="container-form">
-        <form action="register.php" method="post">
-            <label for="name">Nama Institusi</label>
-            <input type="text" name="name" placeholder="Nama Institusi">
-            <label for="username">Username</label>
-            <input type="text" name="username" placeholder="username">
-            <label for="email">Email</label>
-            <input type="email" name="email" placeholder="example@domain.org">
-            <label for="password">Password</label>
-            <input type="password" name="password" placeholder="Password">
-            <label for="konfir-password">Konfirmasi Password</label>
-            <input type="password" name="konfir-password" placeholder="Konfirmasi Password">
-            <input type="submit" name="submit" value="submit">
-        </form>
+<?php
+include('includes/header.php');
+?>
 
-        <p>Sudah punya akun? <a href="../loginPenerbit.php">Login Sekarang</a></p>
-    </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-    <script src="../javascript/darkMode.js"></script>
+<div class="container-form">
+    <form action="register.php" method="post">
+        <label for="name">Nama Institusi</label>
+        <input type="text" name="name" placeholder="Nama Institusi" required>
+        <label for="username">Username</label>
+        <input type="text" name="username" placeholder="username" required>
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="example@domain.org" required>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+               title="Password mengandung paling tidak 1 angka, 1 huruf kapital dan huruf kecil, dan minimal 8 karakter"
+               required>
+
+        <div id="message">
+            <b>Password harus mengandung paling tidak:</b>
+            <p id="letter" class="invalid">1 <b>huruf kecil</b></p>
+            <p id="capital" class="invalid">1 <b>huruf kapital</b></p>
+            <p id="number" class="invalid">1 <b>angka</b></p>
+            <p id="length" class="invalid"><b>8 karakter</b></p>
+        </div>
+
+        <label for="konfir-password">Konfirmasi Password</label>
+        <input type="password" name="konfir-password" placeholder="Konfirmasi Password" required>
+        <input type="submit" name="submit" value="submit">
+    </form>
+
+
+
+    <p>Sudah punya akun? <a href="../loginPenerbit.php">Login Sekarang</a></p>
+</div>
+
+<script src="../javascript/passwordValidation.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<script src="../javascript/darkMode.js"></script>
 </body>
 
 </html>
