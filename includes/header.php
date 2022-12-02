0eb0e991373bfbd8d204ff36c626b32aa3420c9e
AhmadLutfiAlfares <ahmadlutfialfares7@gmail.com>
2022-12-02 22:27:31 +0800

    up

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,6 @@
             if (isset($_SESSION['loginUser'])) {
                 echo "<li><a href='browse.php'>Jelajahi</a></li>";
                 echo "<li><a href='bookmark.php'>Bookmark</a></li>";
-                echo "<li><a href='profil.php'>Profil</a></li>";
                 echo "<li>Dark mode: <span class='change'>OFF</span></li>";
                 echo "<li><a href='includes/logout.php'>Keluar</a></li>";
             } else {
