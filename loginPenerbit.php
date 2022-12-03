41248481781c92dc5224605ec2dfcbd9367fc0d2
Sam Fareez <vsefareez@gmail.com>
2022-12-03 23:08:34 +0800

    update login penerbit

--- loginPenerbit.php
+++ loginPenerbit.php
@@ -55,11 +55,11 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet/login.css">
     <link rel="stylesheet" href="stylesheet/style-header-footer.css"/>
-    <link rel="stylesheet" href="stylesheet/login.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-          integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Gentium+Plus&family=Work+Sans&display=swap" rel="stylesheet"/>
@@ -70,19 +70,42 @@ if (isset($_POST['submit'])) {
 <?php
 include('includes/header.php');
 ?>
-<div class="title">
-    <h1>Login Penerbit</h1>
-</div>
-<div class="container-form">
-    <form action="" method="post">
-        <input type="text" name="email-username" placeholder="Email atau username">
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="submit">
-    </form>
-    <p>Belum punya akun? <a href="publisher/register.php">Sign Up Sekarang</a></p>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div class="card border-0 shadow rounded-3 my-5">
+                <div class="card-body p-4 p-sm-5">
+                    <h5 class="card-title text-center mb-5 fw-light fs-5">Login Penerbit</h5>
+                    <form action="" method="post">
+                        <div class="form-floating mb-3">
+                            <input name="email-username" type="email" class="form-control" id="floatingInput"
+                                   placeholder="name@example.com">
+                            <label for="floatingInput">Email atau username</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input name="password" type="password" class="form-control" id="floatingPassword"
+                                   placeholder="Password">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                        <div class="d-grid">
+                            <button class="btn btn-primary btn-login text-uppercase fw-bold" name="submit"
+                                    value="submit" type="submit">Sign
+                                in
+                            </button>
+                        </div>
+                        <hr class="my-4">
+                        <p>Belum punya akun? <a href="publisher/register.php">Sign Up Sekarang</a></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="javascript/darkMode.js"></script>
 </body>
